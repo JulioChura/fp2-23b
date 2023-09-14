3784a5353e5f1be7494fa43091ee57c67fb290ac
Julio <jchuraaca@unsa.edu.pe>
2023-09-14 18:30:03 -0500

    Usando arreglos para crear y mostrar a los soldados

--- fase01/lab01/VideoJuego.java
+++ fase01/lab01/VideoJuego.java
@@ -1,43 +1,19 @@
 // Laboratorio Nro 1 - Ejercicio 2
 // Autor: Julio
-// Tiempo: 4 mnutos
+// Tiempo: 10 minutos
 // No hubo colaboradores
 import java.util.*;
 public class VideoJuego{
     public static void main(String[] args){
 	Scanner sc = new Scanner(System.in);
-	String soldier1, soldier2, soldier3, soldier4, soldier5;
-	int life1, life2, life3, life4, life5;
 
-        System.out.println("Ingrese el nombre del soldado 1");
-	soldier1 = sc.next();
-        System.out.println("Ingrese el nivel de vida del soldado 1");
-	life1 = sc.nextInt();
-	
-        System.out.println("Ingrese el nombre del soldado 2");
-	soldier2 = sc.next();
-        System.out.println("Ingrese el nivel de vida del soldado 2");
-	life2 = sc.nextInt();
-
-        System.out.println("Ingrese el nombre del soldado 3");
-	soldier3 = sc.next();
-        System.out.println("Ingrese el nivel de vida del soldado 3");
-	life3 = sc.nextInt();
-	
-        System.out.println("Ingrese el nombre del soldado 4");
-	soldier4 = sc.next();
-        System.out.println("Ingrese el nombre del soldado 4");
-	life4 = sc.nextInt();
-
-        System.out.println("Ingrese el nombre del soldado 5");
-	soldier5 = sc.next();
-        System.out.println("Ingrese el nivel de vida del soldado 5");
-	life5 = sc.nextInt();
-
-	System.out.println("El soldado 1 se llama: "+ soldier1 + "  Nivel de vida: "+ life1);     
-	System.out.println("El soldado 2 se llama: "+ soldier2 + "  Nivel de vida: "+ life2);     
-	System.out.println("El soldado 3 se llama: "+ soldier3 + "  Nivel de vida: "+ life3);     
-	System.out.println("El soldado 4 se llama: "+ soldier4 + "  Nivel de vida: "+ life4);     
-	System.out.println("El soldado 5 se llama: "+ soldier5 + "  Nivel de vida: "+ life5);     
+	String[] army = new String[5];
+	for(int i = 0; i < army.length; i++){
+	    System.out.println("Enter the name of soldier "+ i);
+	    army[i] = sc.next();
+	}
+	for(int i = 0; i < army.length; i++){
+	    System.out.println("Soldier: "+ army[i]);
+	}    
    }       
 }
