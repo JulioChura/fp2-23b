51bf4d7a164191820f13fad1d3d6c846ae7fc364
Julio Chura <jchuraaca@unsa.edu.pe>
2023-09-15 15:07:53 -0500

    Enfrentamiento de dos ejercitos

--- fase01/lab01/VideoJuego.java
+++ fase01/lab01/VideoJuego.java
@@ -5,21 +5,37 @@
 import java.util.*;
 public class VideoJuego{
     public static void main(String[] args){
-	Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 
-	String[] army = new String[5];
-	for(int i = 0; i < army.length; i++){
-	    System.out.println("Enter the name of soldier "+ (i+1));
-	    army[i] = sc.next();
+		String[] ejercito1 = army();
+		String[] ejercito2 = army(); 
+	
+		printArray(ejercito1);
+		printArray(ejercito2);
+		if(ejercito1.length < ejercito2.length ){
+			System.out.println("El ganador es el ejercito 2");
+		}else if( ejercito1.length > ejercito2.length){
+			System.out.println("El ganador es el ejercito 1");		
+		}else{
+		System.out.println("Empate");
+		}
 	}
-	for(int i = 0; i < army.length; i++){
-	    System.out.println("Soldier "+(i+1)+": "+ army[i]);
-	    System.out.println("Life: "+ life()+"\t");
+	public static String[] army(){
+		Random aleatorio = new Random();
+		int cantidad = aleatorio.nextInt(5)+1;
+		String[] ejercito = new String[cantidad];
+		String soldado = "";		
+		for(int i = 0; i < ejercito.length; i++){
+			soldado = "soldado"+(i+1);
+			ejercito[i] = soldado;
+		}
+		return ejercito;
 	}
-     
-    }
-    public static int life(){
-    	Random lifePoints = new Random();
-	return lifePoints.nextInt(10)+1;
-    } 
+	
+	public static void printArray(String[] a){
+		for(int i = 0; i < a.length; i++){
+			System.out.println(a[i] );
+		}
+		
+	}
 }
