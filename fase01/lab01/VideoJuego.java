e4909155d9bb3c6f83121e5a48566bb8b738b882
Julio <jchuraaca@unsa.edu.pe>
2023-09-15 18:20:58 -0500

    Ejercicio 5 corregido

--- fase01/lab01/VideoJuego.java
+++ fase01/lab01/VideoJuego.java
@@ -1,41 +1,45 @@
-// Laboratorio Nro 1 - Ejercicio 2
+// Laboratorio Nro 1 - Ejercicio 5
 // Autor: Julio
-// Tiempo: 8 minutos
+// Tiempo: 20 minutos
 // No hubo colaboradores
 import java.util.*;
 public class VideoJuego{
     public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+	
+	String[] army1 = generateArmy();
+	String[] army2 = generateArmy(); 
 
-		String[] ejercito1 = army();
-		String[] ejercito2 = army(); 
+	System.out.println("ARMY 1");
+	printArray(army1);
+	System.out.println("ARMY 2");
+	printArray(army2);
+
+	if (army1.length < army2.length ) {
+	    System.out.println("The winner is the army 2");
+	} else if ( army1.length > army2.length) {
+	    System.out.println("The winner is the army 1");		
+	} else {
+	    System.out.println("IT WAS A TIE");
+	}
+    }
+
+    public static String[] generateArmy(){
+        Random random = new Random();
+	int amount = random.nextInt(5)+1;
+	String[] army = new String[amount];
+	String soldier = "";		
+	for(int i = 0; i < army.length; i++){
+	    soldier = "Soldier"+(i+1);
+	    army[i] = soldier;
+	}
+	return army;
+    }
 	
-		printArray(ejercito1);
-		printArray(ejercito2);
-		if(ejercito1.length < ejercito2.length ){
-			System.out.println("El ganador es el ejercito 2");
-		}else if( ejercito1.length > ejercito2.length){
-			System.out.println("El ganador es el ejercito 1");		
-		}else{
-		System.out.println("Empate");
-		}
+    public static void printArray(String[] a){
+	for(int i = 0; i < a.length; i++){
+	    System.out.println(a[i] );
 	}
-	public static String[] army(){
-		Random aleatorio = new Random();
-		int cantidad = aleatorio.nextInt(5)+1;
-		String[] ejercito = new String[cantidad];
-		String soldado = "";		
-		for(int i = 0; i < ejercito.length; i++){
-			soldado = "soldado"+(i+1);
-			ejercito[i] = soldado;
-		}
-		return ejercito;
-	}
-	
-	public static void printArray(String[] a){
-		for(int i = 0; i < a.length; i++){
-			System.out.println(a[i] );
-		}
-		
-	}
+	System.out.println();		
+    }
 }
