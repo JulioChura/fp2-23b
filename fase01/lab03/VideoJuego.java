3cfa47e4375bde82da02dd80c202abd4916b0956
Julio <jchuraaca@unsa.edu.pe>
2023-09-23 17:07:20 -0500

    Ejercicio 05 de la practica 01 de lab culminada

--- fase01/lab03/VideoJuego.java
+++ fase01/lab03/VideoJuego.java
@@ -1,29 +1,39 @@
-// Laboratorio Nro 1 - Ejercicio 4
+// Laboratorio Nro 1 - Ejercicio 5
 // Autor: Julio
-// Tiempo: 30 minutos
+// Tiempo: 20 minutos
 // No hubo colaboradores
 import java.util.*;
 public class VideoJuego{
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        Soldier soldier = new Soldier();
-	Soldier[] army = new Soldier[5];
-	for(int i = 0; i < army.length; i++){
-            System.out.print("Enter the name of soldier "+(i+1)+": " );
-            String name = sc.next();
-            System.out.print("Enter the points life "+(i+1)+": " );
-            int points = sc.nextInt();
-            System.out.println();
-            army[i] = new Soldier();
-            army[i].setName(name);
-            army[i].setLifePoints(points);           
-        }
-        System.out.println();	
-        printArray(army);
+	Soldier[] army1 = generateArmy();
+	Soldier[] army2 = generateArmy();
+
+	printArray(army1);
+	System.out.println();	
+        printArray(army2);
+	if (army1.length < army2.length ) {
+	    System.out.println("The winner is the army 2");
+	} else if ( army1.length > army2.length) {
+	    System.out.println("The winner is the army 1");		
+	} else {
+	    System.out.println("IT WAS A TIE");
+	}	
     }   
     public static void printArray(Soldier[] a){
 	for(int i = 0; i < a.length; i++){
 	    System.out.println(a[i].toString() );
 	}	
     }
+    public static Soldier[] generateArmy(){
+    	Random random = new Random();
+	int amount =  random.nextInt(5)+1;
+	Soldier[] myArmy = new Soldier[amount];
+	for(int i = 0; i < myArmy.length; i++){
+  	    String theName = "Soldier"+i;
+	    myArmy[i] = new Soldier();
+	    myArmy[i].setName(theName);
+	}
+	return myArmy;
+    }
+
 }
