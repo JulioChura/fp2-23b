2641ac5f8e774ab21fea8b5e110250d6a849a469
Julio <jchuraaca@unsa.edu.pe>
2023-09-27 16:29:51 -0500

    Corrigiendo la identación

--- fase01/lab03/VideoJuego.java
+++ fase01/lab03/VideoJuego.java
@@ -5,35 +5,38 @@
 import java.util.*;
 public class VideoJuego{
     public static void main(String[] args){
-	Soldier[] army1 = generateArmy();
-	Soldier[] army2 = generateArmy();
+		Soldier[] army1 = generateArmy();
+		Soldier[] army2 = generateArmy();
+	
+		printArray(army1);
+		System.out.println();	
+   		printArray(army2);
+	
+		if (army1.length < army2.length ) {
+	    	System.out.println("The winner is the army 2");
+		} else if ( army1.length > army2.length) {
+	    	System.out.println("The winner is the army 1");		
+		} else {
+	   	 	System.out.println("IT WAS A TIE");
+		}	
+    }  
 
-	printArray(army1);
-	System.out.println();	
-        printArray(army2);
-	if (army1.length < army2.length ) {
-	    System.out.println("The winner is the army 2");
-	} else if ( army1.length > army2.length) {
-	    System.out.println("The winner is the army 1");		
-	} else {
-	    System.out.println("IT WAS A TIE");
-	}	
-    }   
     public static void printArray(Soldier[] a){
-	for(int i = 0; i < a.length; i++){
+		for(int i = 0; i < a.length; i++){
 	    System.out.println(a[i].toString() );
-	}	
+		}	
     }
+
     public static Soldier[] generateArmy(){
     	Random random = new Random();
-	int amount =  random.nextInt(5)+1;
-	Soldier[] myArmy = new Soldier[amount];
-	for(int i = 0; i < myArmy.length; i++){
-  	    String theName = "Soldier"+i;
-	    myArmy[i] = new Soldier();
-	    myArmy[i].setName(theName);
+		int amount =  random.nextInt(5)+1;
+		Soldier[] myArmy = new Soldier[amount];
+		for(int i = 0; i < myArmy.length; i++){
+  	    	String theName = "Soldier"+i;
+	    	myArmy[i] = new Soldier();
+	    	myArmy[i].setName(theName);
+		}	
+		return myArmy;
 	}
-	return myArmy;
-    }
 
 }
