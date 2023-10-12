35ba97c6a5662cab8554218ade4b1d5083301911
Julio <jchuraaca@unsa.edu.pe>
2023-10-12 16:02:19 -0500

    Se reemplaza el metodo de imprimir por el de generar arreglo unidimensional para poder trabajar los demas metodos

--- fase01/lab05/VideoJuego.java
+++ fase01/lab05/VideoJuego.java
@@ -9,18 +9,31 @@ public class VideoJuego {
 	public static void main(String[] args) {
 
 		Soldier[][] army = generateArmy();
-		printArray(army);
+		Soldier[] armyU = arrayUnidimensional(army);
 		miTablero(army);
 	}
 
-	public static void printArray(Soldier[][] a) {
-		for (int i = 0; i < a.length; i++) {
-			for (int j = 0; j < a[i].length; j++) {
-				if (a[i][j] != null) {
-					System.out.println(a[i][j].toString());
+	public static Soldier[] arrayUnidimensional(Soldier[][] s) {
+		int contador = 0;
+		for (int i = 0; i < s.length; i++) {
+			for (int j = 0; j < s[i].length; j++) {
+				if (s[i][j] != null) {
+					contador++;
 				}
 			}
 		}
+
+		Soldier[] armyU = new Soldier[contador];
+		int indice = 0;
+		for (int i = 0; i < s.length; i++) {
+			for (int j = 0; j < s[i].length; j++) {
+				if (s[i][j] != null) {
+					armyU[indice] = s[i][j];
+					indice++;
+				}
+			}
+		}
+		return armyU;
 	}
 
 	public static Soldier[][] generateArmy() {
@@ -72,45 +85,6 @@ public class VideoJuego {
 			}
 			System.out.println();
 		}
-	}
-
-	// Se hará uso del ordenamiento por inserción
-	public static void mayorVida(Soldier[][] s) {
-		int contador = 0;
-		for (int i = 0; i < s.length; i++) {
-			for (int j = 0; j < s[i].length; j++) {
-				if (s[i][j] != null) {
-					contador++;
-				}
-			}
-		}
-
-		Soldier[] armyU = new Soldier[contador];
-		int indice = 0;
-		for (int i = 0; i < s.length; i++) {
-			for (int j = 0; j < s[i].length; j++) {
-				if (s[i][j] != null) {
-					armyU[indice] = s[i][j];
-					indice++;
-				}
-			}
-		}
-
-		for (int i = 0; i < armyU.length; i++ ) {
-			int maxIndex = i;
-			for (int j = i + 1; j < armyU.length; j++) {
-				if (armyU[j].getLifePoints() > armyU[maxIndex].getLifePoints()) {
-					maxIndex = j;
-				}
-				Soldier auxiliar = armyU[maxIndex];
-				armyU[maxIndex] = armyU[i];
-				armyU[i] = auxiliar;
-			}
-		}
-		for (int i = 0; i < armyU.length; i++) {
-			System.out.println(armyU[i].toString());
-		}
-
 	}
 
 }
