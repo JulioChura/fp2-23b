caf2f76c7a9b4adf65fe876c62891d0308a1327d
Julio <jchuraaca@unsa.edu.pe>
2023-10-12 14:51:19 -0500

    Metodo de ordenamiento por insercion

--- fase01/lab05/VideoJuego.java
+++ fase01/lab05/VideoJuego.java
@@ -10,7 +10,6 @@ public class VideoJuego {
 
 		Soldier[][] army = generateArmy();
 		printArray(army);
-
 		miTablero(army);
 	}
 
@@ -72,6 +71,44 @@ public class VideoJuego {
 				System.out.print(tablero[i][j]);
 			}
 			System.out.println();
+		}
+	}
+
+	// Se hará uso del ordenamiento por inserción
+	public static void mayorVida(Soldier[][] s) {
+		int contador = 0;
+		for (int i = 0; i < s.length; i++) {
+			for (int j = 0; j < s[i].length; j++) {
+				if (s[i][j] != null) {
+					contador++;
+				}
+			}
+		}
+
+		Soldier[] armyU = new Soldier[contador];
+		int indice = 0;
+		for (int i = 0; i < s.length; i++) {
+			for (int j = 0; j < s[i].length; j++) {
+				if (s[i][j] != null) {
+					armyU[indice] = s[i][j];
+					indice++;
+				}
+			}
+		}
+
+		for (int i = 0; i < armyU.length; i++) {
+			int maxIndex = i;
+			for (int j = i + 1; j < armyU.length; j++) {
+				if (armyU[j].getLifePoints() > armyU[maxIndex].getLifePoints()) {
+					maxIndex = j;
+				}
+				Soldier auxiliar = armyU[maxIndex];
+				armyU[maxIndex] = armyU[i];
+				armyU[i] = auxiliar;
+			}
+		}
+		for (int i = 0; i < armyU.length; i++) {
+			System.out.println(armyU[i].toString());
 		}
 
 	}
