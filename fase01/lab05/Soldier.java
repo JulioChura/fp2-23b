23cfc5dad0fd905d54993da867b2d7172b9f7c67
Julio <jchuraaca@unsa.edu.pe>
2023-10-11 20:43:47 -0500

    La clase Soldier.java tiene los atributos y los métodos solicitados en el ejercicio

--- fase01/lab05/Soldier.java
+++ fase01/lab05/Soldier.java
@@ -1,13 +1,23 @@
 public class Soldier {
     private String name;
     private int lifePoints;
-    
+    private int row;
+    private int column;
+
     public String getName() {
         return name;
     }
 
     public int getLifePoints() {
         return lifePoints;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
     }
 
     public void setName(String name) {
@@ -18,9 +28,16 @@ public class Soldier {
         this.lifePoints = lifePoints;
     }
 
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
     public String toString() {
         return "Soldier{" + "name=" + name + ", lifePoints=" + lifePoints + '}';
-    }  
-   	
-}
+    }
 
+}
