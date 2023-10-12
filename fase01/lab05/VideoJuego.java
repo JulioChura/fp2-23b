7a400810579c84a05b62c790f5e2267ace2b8359
Julio <jchuraaca@unsa.edu.pe>
2023-10-11 22:05:10 -0500

    Se ha creado el metodo que permite generar un arreglo de soldados sin repetir posiciones

--- fase01/lab05/VideoJuego.java
+++ fase01/lab05/VideoJuego.java
@@ -1,13 +1,13 @@
 // Laboratorio Nro 5
 // Autor: Julio
-// Tiempo: 
+// Tiempo: 20
 // No hubo colaboradores
 
 import java.util.*;
 
 public class VideoJuego {
 	public static void main(String[] args) {
-
+		Soldier[][] army = generateArmy();
 	}
 
 	public static void printArray(Soldier[] a) {
@@ -16,15 +16,29 @@ public class VideoJuego {
 		}
 	}
 
-	public static Soldier[] generateArmy() {
+	public static Soldier[][] generateArmy() {
 		Random random = new Random();
-		int amount = random.nextInt(5) + 1;
-		Soldier[] myArmy = new Soldier[amount];
-		for (int i = 0; i < myArmy.length; i++) {
-			String theName = "Soldier" + i;
-			myArmy[i] = new Soldier();
-			myArmy[i].setName(theName);
-		}
+		int amount = random.nextInt(10) + 1;
+		Soldier[][] myArmy = new Soldier[10][10];
+		int i = 0;
+		do {
+			int row = random.nextInt(10);
+			int column = random.nextInt(10);
+			if (myArmy[row][column] == null) {
+				String name = "Soldier" + (i + 1);
+				int lifePoints = random.nextInt(5) + 1;
+
+				myArmy[row][column] = new Soldier();
+
+				myArmy[row][column].setLifePoints(lifePoints);
+				myArmy[row][column].setName(name);
+				myArmy[row][column].setColumn(column);
+				myArmy[row][column].setRow(row);
+
+				i++;
+			}
+		} while (i < amount);
+
 		return myArmy;
 	}
 
