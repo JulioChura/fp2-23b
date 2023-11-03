73ad0ec83d4c273a3169b0c15ec68c6e4dd9d2a2
Julio <jchuraaca@unsa.edu.pe>
2023-11-02 20:21:57 -0500

    Se aumentan atributos nuevos a la clase y tambien los constructores

--- fase02/lab09/Soldier.java
+++ fase02/lab09/Soldier.java
@@ -1,9 +1,38 @@
 public class Soldier {
 
     private String name;
-    private int lifePoints;
     private int row;
     private int column;
+    private int attackLevel;
+    private int defenseLevel;
+    private int lifePoints;
+    private int actualLife;
+    private int speed;
+    private String attitude;
+    private boolean current;
+
+    public Soldier(String name, int row, int column, int attackLevel, int defenseLevel, int lifePoints) {
+        this(name, row, column, attackLevel, defenseLevel, lifePoints, lifePoints, 0, "Reposo", true);
+    }
+
+    public Soldier(String name, int attackLevel, int defenseLevel, int lifePoints, int speed, String attitude,
+            boolean current) {
+        this(name, 0, 0, attackLevel, defenseLevel, lifePoints, lifePoints, speed, attitude, current);
+    }
+
+    public Soldier(String name, int row, int column, int attackLevel, int defenseLevel, int lifePoints, int actualLife,
+            int speed, String attitude, boolean current) {
+        this.name = name;
+        this.row = row;
+        this.column = column;
+        this.attackLevel = attackLevel;
+        this.defenseLevel = defenseLevel;
+        this.lifePoints = lifePoints;
+        this.actualLife = actualLife;
+        this.speed = speed;
+        this.attitude = attitude;
+        this.current = current;
+    }
 
     public String getName() {
         return name;
@@ -36,9 +65,4 @@ public class Soldier {
     public void setColumn(int column) {
         this.column = column + 1;
     }
-
-    public String toString() {
-        return "Soldier [name=" + name + ", lifePoints=" + lifePoints + ", row=" + row + ", column=" + column + "]";
-    }
-
 }
