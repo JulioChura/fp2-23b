1eaab04e81dc9e3fefb9e8c2eb3828b197354723
Julio <jchuraaca@unsa.edu.pe>
2023-12-07 21:18:00 -0500

    Se sube Soldier al lab12

---
+++ fase02/lab12/Soldier.java
@@ -0,0 +1,44 @@
+public class Soldier {
+
+    private String name;
+    private int lifePoints;
+    private int row;
+    private int column;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLifePoints() {
+        return lifePoints;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLifePoints(int lifePoints) {
+        this.lifePoints = lifePoints;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    public String toString() {
+        return "Soldier [name=" + name + ", lifePoints=" + lifePoints + ", row=" + row + ", column=" + column + "]";
+    }
+
+}
