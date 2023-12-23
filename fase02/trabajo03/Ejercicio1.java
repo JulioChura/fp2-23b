cc66850a3558b5119632a008d379f6ca72447992
Julio <jchuraaca@unsa.edu.pe>
2023-12-23 18:45:10 -0500

    El programa fue terminado

--- fase02/trabajo03/Ejercicio1.java
+++ fase02/trabajo03/Ejercicio1.java
@@ -1,5 +1,6 @@
 class Punto {
-    /*Se pone el nivel protected debido a que se
+    /*
+     * Se pone el nivel protected debido a que se
      * aplicará herencia. Esta es la clase padre
      */
     private double x;
@@ -17,12 +18,12 @@ class Punto {
     public double getY() {
         return y;
     }
-    //Este metodo calcula la distancia entre dos puntos
+
+    // Este metodo calcula la distancia entre dos puntos
     public double distancia(Punto p) {
         return Math.sqrt(Math.pow(x - p.getX(), 2) +
-         Math.pow(y - p.getY(), 2));
+                Math.pow(y - p.getY(), 2));
     }
-
 }
 
 class Circulo {
@@ -30,28 +31,29 @@ class Circulo {
     private Punto puntoDestino;
     private double radio;
 
-    public Circulo (Punto p1, Punto p2) {
+    public Circulo(Punto p1, Punto p2) {
         puntoOrigen = p1;
         puntoDestino = p2;
     }
+
+    /*
+     * Para evitar repetir codigo, se hace uso del metodo
+     * distancia de la clase Punto
+     */
     public double calcularRadio() {
         this.radio = puntoOrigen.distancia(puntoDestino);
         return this.radio;
     }
-    public  String toString () {
-        return "Punto de Origen: "+ puntoDestino +
-         " Punto de Destino: "+ puntoDestino + " Radio: "+  radio;
-    }
+
 }
 
 public class Ejercicio1 {
-
     public static void main(String[] args) {
         Punto p1 = new Punto(3, 4);
         Punto p2 = new Punto(0, 0);
         Circulo circulo = new Circulo(p1, p2);
-        circulo.distancia();
-        System.out.println("La distancia es: " + distancia);
+        double radio = circulo.calcularRadio();
+        System.out.println("El radio es: " + radio);
 
     }
 }
