14b1d2bd59262d4f31faaee95413a35ab6633295
Julio <jchuraaca@unsa.edu.pe>
2023-12-23 11:47:47 -0500

    Se crean los atributos de la clase Punto

--- fase02/trabajo03/Ejercicio1.java
+++ fase02/trabajo03/Ejercicio1.java
@@ -1,11 +1,13 @@
 public class Ejercicio1 {
+    /*
+     * Se pone el nivel protected ya que se usará herencia
+     */
     public class Punto {
-
+        protected double  x;
+        protected double y;
     }
     public class Circulo extends Punto {
-
     }
     public static void main(String[] args) {
-        
     }
 }
