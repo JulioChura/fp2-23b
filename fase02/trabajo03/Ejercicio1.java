49096c5deeaa439399b1dfea0f78d01cf427d6cd
Julio <jchuraaca@unsa.edu.pe>
2023-12-23 12:09:05 -0500

    Se crean los metodos accesores

--- fase02/trabajo03/Ejercicio1.java
+++ fase02/trabajo03/Ejercicio1.java
@@ -3,11 +3,30 @@ public class Ejercicio1 {
      * Se pone el nivel protected ya que se usará herencia
      */
     public class Punto {
-        protected double  x;
+        protected double x;
         protected double y;
+
+        public Punto(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public double getX() {
+            return x;
+        }
+
+        public double getY() {
+            return y;
+        }
+
+        public double distancia() {
+            return;
+        }
     }
+
     public class Circulo extends Punto {
     }
+
     public static void main(String[] args) {
     }
 }
