d60fa4e22f01cb9a9c3b302d6f4fe8711ac8e56c
Julio <jchuraaca@unsa.edu.pe>
2023-12-23 22:06:53 -0500

    Se crean mas metodos

---
+++ fase02/trabajo03/Ejercicio3.java
@@ -0,0 +1,21 @@
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        System.out.println("Interfaces");
+    }
+}
+
+interface Barco {
+    void navegar();
+
+    void amarrar();
+}
+
+interface Avion {
+    void volar();
+
+    void aterrizar();
+}
+
+public class Hidroavion {
+
+}
