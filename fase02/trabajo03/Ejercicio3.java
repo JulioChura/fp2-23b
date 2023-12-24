0d088316d5d9833ee26be8acfd4592f1eda38f38
Julio <jchuraaca@unsa.edu.pe>
2023-12-23 22:21:43 -0500

    Se termina  el ejercicio 3

--- fase02/trabajo03/Ejercicio3.java
+++ fase02/trabajo03/Ejercicio3.java
@@ -1,6 +1,11 @@
 public class Ejercicio3 {
     public static void main(String[] args) {
-        System.out.println("Interfaces");
+        Hidroavion hidroavion = new Hidroavion(" Canadair CL-215", "China");
+        hidroavion.amarrar();
+        hidroavion.navegar();
+        hidroavion.volar();
+        hidroavion.aterrizar();
+        System.out.print(hidroavion);
     }
 }
 
@@ -16,6 +21,33 @@ interface Avion {
     void aterrizar();
 }
 
-public class Hidroavion {
+class Hidroavion implements Barco, Avion {
+    private String modelo;
+    private String lugarProcedencia;
+
+    public Hidroavion(String modelo, String lugar) {
+        this.modelo = modelo;
+        this.lugarProcedencia = lugar;
+    }
+
+    public void navegar() {
+        System.out.println("El HidroaviÓn está avanzando en el agua!");
+    }
+
+    public void amarrar() {
+        System.out.println("El Hidroavion se detuve en un muelle!");
+    }
+
+    public void volar() {
+        System.out.println("El hidroavión está volando!");
+    }
+
+    public void aterrizar() {
+        System.out.println("El hidroavión está aterrizando!");
+    }
+
+    public String toString() {
+        return "Hidroavion [modelo=" + modelo + ", Lugar de Procedencia=" + lugarProcedencia + "]";
+    }
 
 }
