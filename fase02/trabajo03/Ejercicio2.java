fb11b2ae2ac83c91e25ce981c97e62fc7d6c02e6
Julio <jchuraaca@unsa.edu.pe>
2023-12-23 19:17:07 -0500

    Se copian las clases Circulo y Punto del Ejercicio1 al 2

---
+++ fase02/trabajo03/Ejercicio2.java
@@ -0,0 +1,50 @@
+public class Ejercicio2 {
+    class Punto {
+        /*
+         * Se establecen como atributos dos valores de tipo
+         * double que seran una coordenada
+         */
+        private double x;
+        private double y;
+    
+        public Punto(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+    
+        public double getX() {
+            return x;
+        }
+    
+        public double getY() {
+            return y;
+        }
+    
+        // Este metodo calcula la distancia entre dos puntos
+        public double distancia(Punto p) {
+            return Math.sqrt(Math.pow(x - p.getX(), 2) +
+                    Math.pow(y - p.getY(), 2));
+        }
+    }
+    
+    class Circulo {
+        private Punto puntoOrigen;
+        private Punto puntoDestino;
+        private double radio;
+    
+        public Circulo(Punto p1, Punto p2) {
+            puntoOrigen = p1;
+            puntoDestino = p2;
+        }
+    
+        /*
+         * Para evitar repetir codigo, se hace uso del metodo
+         * distancia de la clase Punto
+         */
+        public double calcularRadio() {
+            this.radio = puntoOrigen.distancia(puntoDestino);
+            return this.radio;
+        }
+    
+    }
+}
