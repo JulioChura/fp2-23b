032ee7d8c77a03aa38a8005972523432dd1fecf2
Julio <jchuraaca@unsa.edu.pe>
2024-01-10 11:12:23 -0500

    Se crean los atributos de la clase Army

---
+++ fase02/lab20/Army.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Army {
+
+    private ArrayList<ArrayList<Soldier>> army;
+    private ArrayList<Soldier> armyU;
+    private String name;
+
+    private static final int COLUMN_BOARD = 10;
+    private static final int ROW_BOARD = 10;
+
+}
