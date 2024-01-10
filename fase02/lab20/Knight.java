910bc0b71ace8854ca7486725ba48bcf22a1d191
Julio <jchuraaca@unsa.edu.pe>
2024-01-09 22:03:21 -0500

    Se crea la clase knight

---
+++ fase02/lab20/Knight.java
@@ -0,0 +1,66 @@
+public class Knight extends Soldier {
+    private String currentWeapon;
+    private boolean horseRiding;
+
+    public Knight(String name, int row, int column, int attackLevel, int defenseLevel, int actualLife, int speed,
+            String attitude, boolean current, int arroes, String weapon, boolean riding) {
+        super(name, row, column, attackLevel, defenseLevel, actualLife, speed, attitude, current);
+        currentWeapon = weapon;
+        horseRiding = riding;
+    }
+
+    
+
+    public Knight(String currentWeapon, boolean horseRiding) {
+        this.currentWeapon = currentWeapon;
+        this.horseRiding = horseRiding;
+    }
+
+
+
+    public Knight(String name, int row, int column, int attackLevel, int defenseLevel, int actualLife, int speed,
+            String currentWeapon, boolean horseRiding) {
+        super(name, row, column, attackLevel, defenseLevel, actualLife, speed);
+        this.currentWeapon = currentWeapon;
+        this.horseRiding = horseRiding;
+    }
+
+
+
+    public void envestir() {
+        if (horseRiding == true)
+            for (int i = 0; i <= 2; i++)
+                super.attack();
+        else
+            super.attack();
+    }
+
+    public void desmontar() {
+        if (horseRiding == true) {
+            horseRiding = false;
+            super.defend();
+            cambiaArma();
+        }
+    }
+
+    public void cambiaArma() {
+        if (currentWeapon == "Lanza")
+            currentWeapon = "Espada";
+        else
+            currentWeapon = "Lanza";
+    }
+
+    public void montar() {
+        if (horseRiding == false) {
+            horseRiding = true;
+            super.attack();
+            cambiaArma();
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        return "Knight: "+ super.toString() + ", currentWeapon=" + currentWeapon + ", horseRiding=" + horseRiding + "]";
+    }
+}
