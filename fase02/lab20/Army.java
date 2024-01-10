2f415f95bd8470ae0b16e5a2c7af7d2fdfb1a841
Julio <jchuraaca@unsa.edu.pe>
2024-01-10 11:20:00 -0500

    Se crean 3 constructores sobrecargados

--- fase02/lab20/Army.java
+++ fase02/lab20/Army.java
@@ -9,4 +9,23 @@ public class Army {
     private static final int COLUMN_BOARD = 10;
     private static final int ROW_BOARD = 10;
 
+    public Army(String name) {
+        ArrayList<ArrayList<Soldier>> army = new ArrayList<>(10);
+        for (int i = 0; i < 10; i++) {
+            army.add(new ArrayList<>(Collections.nCopies(10, null)));
+        }
+        this.army = army;
+        this.name = name;
+    }
+
+    public Soldier getSoldier(int row, int column) {
+        return army.get(row).get(column);
+    }
+
+    public void setSoldier(int row, int column, Army a, Soldier sol) {
+        army.get(row).set(column, sol);
+
+    }
+
+    
 }
