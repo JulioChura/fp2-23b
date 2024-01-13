16144c6bcaae203a95ebd760bf476761040117fb
Julio <jchuraaca@unsa.edu.pe>
2024-01-13 14:05:23 -0500

    Se copia generateBoard de la clase Board y cuando impimimos el tablero solo se muestra la matriz de botones sin laas letras que se asignaron a cada boton

--- fase02/lab22/Tablero.java
+++ fase02/lab22/Tablero.java
@@ -1,8 +1,9 @@
 import javax.swing.*;
-
+import java.util.*;
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.Panel;
+import java.util.ArrayList;
 
 public class Tablero extends JFrame {
 
@@ -11,24 +12,84 @@ public class Tablero extends JFrame {
 
     private JButton[][] casillas;
 
-    public Tablero(int filas, int columnas) {
-        casillas = new JButton[filas][columnas];
-        setSize(500,500);
+    public Tablero(Army army1, Army army2, String kingdom1, String kingdom2) {
+
+        casillas = new JButton[ROW][COLUMN];
+        setSize(500, 500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("Tablero de Juego");
-        setLayout(new GridLayout(filas, columnas));
+        setLayout(new GridLayout(ROW, COLUMN));
 
-        for (int i = 0; i < filas; i++) {
-            for (int j = 0; j < columnas; j++) {
+        ArrayList<ArrayList<Soldier>> a = army1.getArmyInArrayListBi();
+        ArrayList<ArrayList<Soldier>> b = army2.getArmyInArrayListBi();
+
+        casillas = new JButton[ROW][COLUMN];
+
+        for (int i = 0; i < casillas.length; i++) {
+            for (int j = 0; j < casillas[i].length; j++) {
                 JButton boton = new JButton();
                 casillas[i][j] = boton;
                 add(boton);
             }
         }
+
+        for (int i = 0; i < a.size(); i++) {
+            for (int j = 0; j < a.get(i).size(); j++) {
+                if (a.get(i).get(j) != null) {
+                    JButton boton = new JButton("a");
+                    casillas[i][j] = boton;
+                    add(boton);
+                }
+            }
+        }
+        for (int i = 0; i < b.size(); i++) {
+            for (int j = 0; j < b.get(i).size(); j++) {
+                if (b.get(i).get(j) != null) {
+                    JButton boton = new JButton("b");
+                    casillas[i][j] = boton;
+                    add(boton);
+                }
+            }
+        }
+
+        /*
+         * System.out.print(
+                "      A        B           C          D            E           F             G            H           I             J \n");
+        for (int i = 0; i < map.length; i++) {
+            System.out.printf("%2d", (i + 1));
+            for (int j = 0; j < map[i].length; j++) {
+                System.out.print(map[i][j]);
+            }
+            System.out.println();
+        }
+         * 
+         * 
+         * 
+         */
+        
+
         setVisible(true);
     }
 
     public static void main(String[] args) {
-        Tablero tab = new Tablero(10,10);
+        String kingdom1;
+        String kingdom2;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Elija su reino (Francia, Inglaterra, Moros, Roma, Castilla)");
+            kingdom1 = sc.next();
+
+            System.out.println("Elija su reino (Francia, Inglaterra, Moros, Roma, Castilla)");
+            kingdom2 = sc.next();
+
+            Army a = new Army(kingdom1);
+            Army b = new Army(kingdom2);
+
+            Tablero tab = new Tablero(a, b, kingdom1, kingdom2);
+
+
+
+        
     }
+    
+
 }
