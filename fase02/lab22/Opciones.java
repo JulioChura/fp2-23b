12bcfb033ace50148677b3da6ffbc1362e292835
Julio <jchuraaca@unsa.edu.pe>
2024-01-12 22:10:58 -0500

    Se cambia el JLabel para poner las instrucciones de juego por un JTextField ya que el JLabel no reconoce los saltos de linea

--- fase02/lab22/Opciones.java
+++ fase02/lab22/Opciones.java
@@ -1,9 +1,3 @@
-import javax.swing.JButton;
-import javax.swing.JCheckBox;
-import javax.swing.JComboBox;
-import javax.swing.JFileChooser;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.*;
 
 import java.awt.BorderLayout;
@@ -11,7 +5,7 @@ import java.awt.GridLayout;
 import java.awt.Panel;
 
 public class Opciones extends JFrame {
-    private static final int ANCHO = 400;
+    private static final int ANCHO = 470;
     private static final int LARGO = 300;
 
 
@@ -23,7 +17,7 @@ public class Opciones extends JFrame {
 
     private JComboBox oponente2;
 
-    private static JLabel instrucciones;
+    private static JTextArea instrucciones;
 
     private static JLabel titulo;
     private JPanel arena;
@@ -38,7 +32,7 @@ public class Opciones extends JFrame {
         setTitle("Opciones para jugar");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
-
+        setResizable(false);
         norte = new JPanel();
         norte.setLayout(new GridLayout(2,0));
         regresar = new JButton("Regresar");
@@ -49,21 +43,27 @@ public class Opciones extends JFrame {
         norte.setVisible(true);
 
         
-        instrucciones = new JLabel("1.- En esta sección se escogen los reinos\n 2.- Ambos jugadores deben seleccionar una arena para poder jugar, la arena definitiva sera seleccionada al azar");
+        instrucciones = new JTextArea("1.- En esta sección se escogen los reinos\n 2.- Ambos jugadores deben seleccionar una arena para poder jugar, la arena definitiva sera seleccionada al azar");
+        instrucciones.setLineWrap(true); // Activa el ajuste de línea automático
+        instrucciones.setWrapStyleWord(true); // Ajusta las líneas en palabras completas
+        instrucciones.setEditable(false);
 
         oponente1 = new JComboBox<>(new String[] {"1: Inglaterra", "2: Moros", "3: Roma", "4: Castilla", "5: Francia" });
         oponente2 = new JComboBox<>(new String[] {"1: Inglaterra", "2: Moros", "3: Roma", "4: Castilla", "5: Francia" });
 
+
+
+        
         
         arena = new JPanel();
         arena.setVisible(true);
         titulo = new JLabel("Elija una arena");
         arena.setLayout(new GridLayout(5,1));
-        playa = new JCheckBox("Playa");
-        bosque = new JCheckBox("Bosque");
-        montaña = new JCheckBox("Montaña");
-        campo = new JCheckBox("Campo");
-        desierto = new JCheckBox("Desierto");
+        playa = new JCheckBox("Playa (Le da ventaja a Roma)");
+        bosque = new JCheckBox("Bosque (Le da ventaja a Inglaterra y Roma)");
+        montaña = new JCheckBox("Montaña (Le da ventaja a Castilla)");
+        campo = new JCheckBox("Campo (Le da ventaja a Francia y Roma) ");
+        desierto = new JCheckBox("Desierto (Le da ventaja a los Moros)");
         arena.add(titulo);
         arena.add(playa);
         arena.add(montaña);
