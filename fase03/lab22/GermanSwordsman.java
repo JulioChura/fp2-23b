4a8f2a43331921fd43e829c6fb3bd28c43b76541
Julio <jchuraaca@unsa.edu.pe>
2024-01-19 14:22:33 -0500

    Se termina de crear la clase GermanSwordsman

--- fase03/lab22/GermanSwordsman.java
+++ fase03/lab22/GermanSwordsman.java
@@ -1,4 +1,43 @@
-public class  GermanSwordsman extends Swordsman implements SpecialUnit {
+//Espadachin romano
+public class GermanSwordsman extends Swordsman implements SpecialUnit {
+
     private int javelin = 10;
+
+    public GermanSwordsman(double longSnow, int javelin) {
+        super(longSnow);
+        this.javelin = javelin;
+    }
+
+    public GermanSwordsman(String name, int row, int column, int attackLevel, int defenseLevel, int actualLife,
+            int speed, String attitude, boolean current, double longSnow, int javelin) {
+        super(name, row, column, attackLevel, defenseLevel, actualLife, speed, attitude, current, longSnow);
+        this.javelin = javelin;
+        actualLife = 13;
+    }
+
+    public GermanSwordsman(String name, int row, int column, int attackLevel, int defenseLevel, int actualLife,
+            int speed, double longSnow, boolean shieldWalls, int javelin) {
+        super(name, row, column, attackLevel, defenseLevel, actualLife, speed, longSnow, shieldWalls);
+        this.javelin = javelin;
+        actualLife = 13;
+    }
+
+    public void throwObjects() {
+        if (0 < javelin) {
+            attack();
+            javelin--;
+        } else {
+            System.err.println("No hay javalinas");
+        }
+    }
+
+    public void evolve() {
+        if (levelEvolution <= MAX_LEVEL) {
+            levelEvolution++;
+            javelin =+ 10;
+        } else {
+            System.err.println("Se alcanzo el maximo nivel");
+        }
+    }
 
 }
