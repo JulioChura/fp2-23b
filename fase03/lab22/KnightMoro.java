7b08c6a9f49ffb37c5cbc874838d632493097c57
Julio <jchuraaca@unsa.edu.pe>
2024-01-19 12:49:53 -0500

    Ahora KnightMoro hereda de clase Knight y no directamente de Soldier

--- fase03/lab22/KnightMoro.java
+++ fase03/lab22/KnightMoro.java
@@ -1,35 +1,32 @@
-public class KnightMoro extends Soldier implements SpecialUnit {
+public class KnightMoro extends Knight implements SpecialUnit {
     private int arrows = 10;
     private int levelEvolution = 1;
 
     
-    
-    public KnightMoro(int arrows, int levelEvolution) {
-        this.arrows = arrows;
-        this.levelEvolution = levelEvolution;
-    }
-
-
 
     public KnightMoro(String name, int row, int column, int attackLevel, int defenseLevel, int actualLife, int speed,
-            int arrows, int levelEvolution) {
-        super(name, row, column, attackLevel, defenseLevel, actualLife, speed);
+            String attitude, boolean current, int arroes, String weapon, boolean riding, int arrows,
+            int levelEvolution) {
+        super(name, row, column, attackLevel, defenseLevel, actualLife, speed, attitude, current, arroes, weapon,
+                riding);
         this.arrows = arrows;
         this.levelEvolution = levelEvolution;
         actualLife = 13;
     }
 
-
+    public KnightMoro(String currentWeapon, boolean horseRiding, int arrows, int levelEvolution) {
+        super(currentWeapon, horseRiding);
+        this.arrows = arrows;
+        this.levelEvolution = levelEvolution;
+    }
 
     public KnightMoro(String name, int row, int column, int attackLevel, int defenseLevel, int actualLife, int speed,
-            String attitude, boolean current, int arrows, int levelEvolution) {
-        super(name, row, column, attackLevel, defenseLevel, actualLife, speed, attitude, current);
+            String currentWeapon, boolean horseRiding, int arrows, int levelEvolution) {
+        super(name, row, column, attackLevel, defenseLevel, actualLife, speed, currentWeapon, horseRiding);
         this.arrows = arrows;
         this.levelEvolution = levelEvolution;
         actualLife = 13;
     }
-
-
 
     public void throwObjects() {
         if (arrows > 0) {
@@ -44,9 +41,9 @@ public class KnightMoro extends Soldier implements SpecialUnit {
     public void evolve() {
         if (levelEvolution <= MAX_LEVEL) {
             levelEvolution++;
-            arrows +=  10;
+            arrows += 10;
         }
-    
+
     }
 
 }
