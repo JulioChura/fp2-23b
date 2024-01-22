845a633aa62bbc0c2798d0b097198bac23df51bb
Julio <jchuraaca@unsa.edu.pe>
2024-01-22 10:24:40 -0500

    Se crea un JFrame que contedra otros paneles

---
+++ fase03/lab22/PrincipalFrame.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+public class PrincipalFrame {
+    private static int HEIGHT =  1500;
+    private static int WIDTH = 900;
+    
+    private InformationFrame panelBlue;
+    private InformationFrame panelRed;
+    private Tablero panelGame;
+
+    private JPanel panel;
+    private ImagePanel panelImg;
+
+    private Army armyBlue;
+    private Army armyRed;
+    private String field;
+    private Tablero tablero;
+
+    private JPanel panelData;
+}
